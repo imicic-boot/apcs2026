5d75702cde1fdadc2bff66539afb0ab160ae3258
imicic-boot <imicic@stu.naperville203.org>
2026-08-24 14:11:00 -0500

    Added Output
    We added variables together and showed it in the program using println

--- Chapter1/lessons/Budget/WeeklyBudget.java
+++ Chapter1/lessons/Budget/WeeklyBudget.java
@@ -33,5 +33,12 @@ public class WeeklyBudget
         System.out.print("Enter the number of school lunches you ordered per week #");
         lunchesPerWeek = scan.nextInt();
         
+        // Perform calculations 
+        totalCost = lunchPrice * lunchesPerWeek;
+        remaining = allowance - totalCost;
+        
+        System.out.println(allowance);
+        System.out.println(totalCost);
+        System.out.println(remaining);
     }
 }
